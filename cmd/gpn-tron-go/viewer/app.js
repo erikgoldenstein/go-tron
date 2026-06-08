5b20f2a1afdee11589ac1d4c6f4f8b8655a2232d
Yannis Gerlach <100762533+ygerlach@users.noreply.github.com>
2026-06-08 16:02:27 +0200

    remove talks
    didnt work anyway

--- cmd/gpn-tron-go/viewer/app.js
+++ cmd/gpn-tron-go/viewer/app.js
@@ -239,32 +239,3 @@ function renderChart() {
     legendX += ctx.measureText(name).width + 12;
   }
 }
-
-async function talks() {
-  try {
-    const response = await fetch('https://cfp.gulas.ch/gpn22/schedule/v/0.26/widget/v2.json').then((r) => r.json());
-    const now = new Date();
-    const upcoming = response.talks.map((talk) => {
-      const room = response.rooms.find((x) => x.id === talk.room) || {};
-      return {
-        title: typeof talk.title === 'string' ? talk.title : (talk.title.de || talk.title.en),
-        room: typeof room.name === 'string' ? room.name : (room.name?.de || room.name?.en || 'Unknown'),
-        start: new Date(talk.start),
-        end: new Date(talk.end),
-      };
-    }).filter((talk) => talk.end > now).sort((a, b) => a.start - b.start);
-
-    const current = upcoming.filter((talk) => talk.start <= now && talk.end > now);
-    let next = upcoming.slice(current.length);
-    if (next.length) next = next.filter((talk) => talk.start < new Date(+next[0].start + 7200000));
-    currentTalks.innerHTML = current.map(talkHTML).join('');
-    nextTalks.innerHTML = next.map(talkHTML).join('');
-  } catch (e) {}
-}
-
-function talkHTML(talk) {
-  return '<div><b>' + esc(talk.title) + '</b><br>' + esc(talk.room) + ' (' + talk.start.toTimeString().split(' ')[0] + ' - ' + talk.end.toTimeString().split(' ')[0] + ')<hr></div>';
-}
-
-talks();
-setInterval(talks, 60000);
